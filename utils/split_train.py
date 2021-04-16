61c1b14f83a3933daafdf666a1e1dd0995ba708f
Pooja <gokhalepooja2000@gmail.com>
2021-04-16 14:36:35 -0400

    train split script added with new files

--- utils/split_train.py
+++ utils/split_train.py
@@ -1,10 +1,16 @@
 import json
 
 PRED_FILE_PATH = '../model_gtt/preds_gtt.out'
-PRED_DIRECTORY_PATH = '../model_gtt/'
+PRED_DIRECTORY_PATH = '../model_gtt/preds_gtt/'
 
 def main():
-  pass
-
+  with open(PRED_FILE_PATH, 'r') as r:
+    train_examples = json.load(r)
+    for key in train_examples:
+      data = train_examples[key]
+      data['docid'] = key
+      with open(PRED_DIRECTORY_PATH + key + '.json', 'w') as w:
+        json.dump(data, w)
+ 
 if __name__ == '__main__':
-  print()
+  main()
